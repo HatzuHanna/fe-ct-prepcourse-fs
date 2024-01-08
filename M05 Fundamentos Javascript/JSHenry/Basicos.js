9691c7e532bfee0460525c173d9ed0c3c5f2d6ce
Juan Martinez HATZU <jmartinez96mol@gmail.com>
2024-01-08 15:18:49 -0500

    Ifs and buts

--- M05 Fundamentos Javascript/JSHenry/Basicos.js
+++ M05 Fundamentos Javascript/JSHenry/Basicos.js
@@ -1,46 +1,83 @@
-var cajon;
+// var cajon;
 
-cajon = 5 + 5;
+// cajon = 5 + 5;
 
-// old way
-console.log("el valor de cajon es: " + cajon);
-//  string interpolado AKA MEJOR
-console.log(`el valor de cajon es: ${cajon}`);
+// // old way
+// console.log("el valor de cajon es: " + cajon);
+// //  string interpolado AKA MEJOR
+// console.log(`el valor de cajon es: ${cajon}`);
 
-console.log("soy un log");
+// console.log("soy un log");
 
-//tarea
-var cajonespar = cajon % 2 == 0;
+// //tarea
+// var cajonespar = cajon % 2 == 0;
 
-console.log(`${cajon} es par? ${cajonespar}`);
+// console.log(`${cajon} es par? ${cajonespar}`);
 
-//funciones
-function sumaTresOG(x) {
-  console.log(x + 3);
-}
+// //funciones
+// function SumaTresOG(x) {
+//   console.log(x + 3);
+// }
 
-//RETURNS
+// //RETURNS
 
-function sumaTres(x) {
-  return x + 3;
-}
+// function SumaTres(x) {
+//   return x + 3;
+// }
 
-cajon = sumaTres(5);
-console.log(cajon);
-cajon = sumaTresOG(5);
-console.log(cajon);
+// cajon = SumaTres(5);
+// console.log(cajon);
+// cajon = SumaTresOG(5);
+// console.log(cajon);
 
-//RETURN TAREA
+// //RETURN TAREA
 
-function esPar(num) {
-  return num % 2 == 0;
-}
+// function EsPar(num) {
+//   return num % 2 == 0;
+// }
 
-esPar(13);
-esPar(12);
+// EsPar(13);
+// EsPar(12);
 
-function sacarPromedio(x, y) {
-  return (x + y) / 2;
-}
+// var variable;
 
-console.log(sacarPromedio(7, 2));
+// function SacarPromedio(x, y) {
+//   return (x + y) / 2;
+// }
+
+// variable = console.log(SacarPromedio(7, 2));
+// console.log(variable);
+
+// var choice = "You chose: ";
+
+// var country = "Argentina";
+
+// function TravelTo(destination) {
+//   if (destination === "Brazil") {
+//     console.log(`${choice}Brazil KKK!!!`);
+//   } else if (destination === "Argentina") {
+//     console.log(`${choice}Cuantas tenes!`);
+//   } else {
+//     console.log(`No tenes copas porque sos  ${destination}`);
+//   }
+// }
+
+// TravelTo(country);
+
+// console.log(`Eligio ${country}`);
+
+// var country = "Argentina";
+
+// function EarlyReturn(destination) {
+//   if (destination === "Brazil") {
+//     console.log("Brazil KKK!!!");
+//     return;
+//   }
+//   if (destination === "Argentina") {
+//     console.log(`Cuantas tenes!`);
+//     return;
+//   }
+//   console.log(`No tenes copas porque sos  ${destination}`);
+// }
+
+// EarlyReturn(country);
