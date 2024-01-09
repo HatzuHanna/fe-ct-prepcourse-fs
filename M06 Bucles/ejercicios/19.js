dab61ab4e5a1e42c89c57418cb534e9205364161
Juan Martinez HATZU <jmartinez96mol@gmail.com>
2024-01-09 15:53:05 -0500

    for desarmado

--- M06 Bucles/ejercicios/19.js
+++ M06 Bucles/ejercicios/19.js
@@ -2,6 +2,19 @@ function sumarHastaN(n) {
   // La función recibe un número "n" por argumento.
   // Devuelve la suma de todos los números desde 1 hasta n.
   // Tu código:
+  if (n < 0) {
+    return 0;
+  }
+  let suma = 0;
+  // for (let i = 1; i <= n; i++) {
+  for (let i = n; i >= 1; i--) {
+    suma += i;
+  }
+  return suma;
+  // if (n <= 0) {
+  //   return 0;
+  // }
+  // return sumarHastaN(n - 1) + n;
 }
 
 module.exports = sumarHastaN;
--- M06 Bucles/ejercicios/20.js
+++ M06 Bucles/ejercicios/20.js
@@ -3,6 +3,30 @@ function sumarHastaNConBreak(n) {
   // Devuelve la suma de todos los números desde 1 hasta n.
   // Si la suma supera a 100, detén el bucle usando break.
   // Tu código:
+
+  if (n <= 0) {
+    return 0;
+  }
+  // let suma = 0;
+  // var i = 1;
+  // for (;;) {
+  //   if (i > n) {
+  //     break;
+  //   }
+  //   suma += i;
+  //   if (suma >= 100) {
+  //     break;
+  //   }
+  //   i++;
+  // }
+  // return suma;
+
+  let suma = 0;
+  for (let i = 1; i <= n && suma < 100; i++) {
+    suma += i;
+  }
+  return suma;
 }
 
+console.log(sumarHastaNConBreak(15));
 module.exports = sumarHastaNConBreak;
