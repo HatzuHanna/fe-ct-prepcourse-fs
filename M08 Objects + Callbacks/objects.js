c3d730550d0d86997049d240e9fbf1cdbe7133e7
Juan Martinez HATZU <jmartinez96mol@gmail.com>
2024-01-11 17:18:41 -0500

    no debugger issue with array

---
+++ M08 Objects + Callbacks/objects.js
@@ -0,0 +1,20 @@
+var deportes = {
+  conBalon: ["Futbol", "Basketball", "Golf"],
+  sinBalon: ["Boxeo", "Surf", "Trekking"],
+};
+var persona = {
+  nombre: "Lucas",
+  edad: 26,
+  estudios: { esProgramador: true, esPintor: false },
+};
+
+persona.nombre = "Martin";
+
+persona.edad = 32;
+
+var autos = {};
+
+autos.marcas = ["Ford", "Audi", "Ferrari"];
+
+console.log(autos);
+console.log("dd");
